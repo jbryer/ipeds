c2b1d601d6db859f00a68b32cd772b46cdf2f834
Jason Bryer <jason@bryer.org>
2012-11-04 08:00:27 -0500

    Initial version.

---
+++ ipeds.dev.R
@@ -0,0 +1,18 @@
+require(devtools)
+require(roxygen2)
+
+setwd("c:/Dropbox/My Dropbox/Projects")
+setwd("~/Dropbox/Projects/")
+
+document("ipeds")
+check_doc('ipeds')
+build("ipeds", path='..')
+build("ipeds", path='..', binary=TRUE)
+check("ipeds")
+install("ipeds")
+
+library(ipeds)
+data(surveys)
+ls('package:ipeds')
+system.file(package='ipeds')
+downloadAllSurveys(2010)
